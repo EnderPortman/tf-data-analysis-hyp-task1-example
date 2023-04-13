c505b49173d056d4b3bf102389b96d5b4f0ab193
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2023-04-13 19:33:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm 
+import statsmodels.api as sm
 
 
 chat_id = 192196854 
@@ -8,13 +8,5 @@ def solution(x_success: int, # usually
              x_cnt: int, #usually
              y_success: int, #test
              y_cnt: int) -> bool: #test
-    p1 = x_success/x_cnt
-    p2 = y_success/y_cnt
-    p = (x_success + y_success)/(x_cnt + y_cnt)
-    z = (p2 - p1)/((p * (1-p) * (1/x_cnt + 1/y_cnt))**0.5)
-    p_val = norm.cdf(-abs(z)) * 2
-    if p_val < 0.08:
-      r = True
-    else:
-      r = False
-    return r
+    z, p = sm.stats.proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
+    return p < 0.08
