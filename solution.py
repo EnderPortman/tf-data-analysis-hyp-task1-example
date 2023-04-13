84ee4baf590f778a35cf766ef56adb003cb1f4bb
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2023-04-13 23:28:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,4 +9,4 @@ def solution(x_success: int, # usually
              y_success: int, #test
              y_cnt: int) -> bool: #test
     z, p = sm.stats.proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
-    return p < 0.08
+    return p <= 0.08
